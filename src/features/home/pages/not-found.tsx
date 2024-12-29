25886c460e39d04ae4e798250f006d62538fe162
borodinka <sofibor03@gmail.com>
2024-12-29 02:35:53 +0100

    feat: add not found page

--- src/features/home/pages/not-found.tsx
+++ src/features/home/pages/not-found.tsx
@@ -1,12 +1,39 @@
-import { Link } from "react-router-dom";
+import { Link, Stack, Typography } from "@mui/material";
+
+import { AppRoutes } from "@config/routes";
+import AppButton from "@features/ui/AppButton";
+import { useBreakpoints } from "@hooks/useBreakpoints";
+
+import NotFoundImage from "../assets/not-found.png";
 
 export default function NotFound() {
+  const { md } = useBreakpoints();
+
   return (
-    <div>
-      <h2>Nothing to see here!</h2>
-      <p>
-        <Link to="/">Go to the home page</Link>
-      </p>
-    </div>
+    <Stack
+      sx={{
+        width: "100vw",
+        height: "90vh",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 2,
+        p: 3,
+      }}
+    >
+      <Typography variant="h1" color="text.secondary">
+        This page is out of this world!
+      </Typography>
+      <Typography textAlign="center">
+        Let's navigate back to safety. Click below to return home
+      </Typography>
+      <img
+        src={NotFoundImage}
+        alt="Hand shake"
+        style={{ width: md ? 550 : 380 }}
+      />
+      <AppButton LinkComponent={Link} href={AppRoutes.home} sx={{ mt: 2 }}>
+        Go Home
+      </AppButton>
+    </Stack>
   );
 }
